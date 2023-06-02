9263e95a498dffb8ccbd4db3ddb24b16e1a723d8
HABT29 <henry.triebel@gmail.com>
2023-06-02 11:55:03 +0200

    lint

--- play.by.play/tests/testthat/test-newapp.R
+++ play.by.play/tests/testthat/test-newapp.R
@@ -3,8 +3,6 @@ library("play.by.play")
 context("Test the game_data function")
 
 test_that("the game_data function returns df of correct size", {
-
-
   gd <- game_data(22200002)
 
   expect_equal(nrow(gd), 634)
